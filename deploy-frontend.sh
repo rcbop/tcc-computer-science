bdf7e335c74c7bf98e7fbcf27b27382f46e4be5d
Rogério Peixoto <rcbpeixoto@gmail.com>
2019-11-28 20:13:04 -0300

    fixes

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -5,5 +5,5 @@ AWSCLI_PROFILE='roger2'
 S3_ACL='public-read'
 CDN_DISTRIBUTION_ID='E2U22FZ5L89H9D'
 
-aws s3 sync public/. s3://$S3_BUCKET/ --region $S3_REGION --acl $S3_ACL --profile $AWSCLI_PROFILE
+aws s3 sync ./frontend/public/. s3://$S3_BUCKET/ --region $S3_REGION --acl $S3_ACL --profile $AWSCLI_PROFILE
 aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
